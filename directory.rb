6f85423a8a709b99786f6ebfef2897ca64d4d2fb
Alex Withington-Smith <alex@awithsmith.com>
2020-10-11 10:12:34 +0100

    emunerated over array instead of repeating

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 # students in array
 students = [
   "Dr. Hannibal Lecter",
@@ -17,17 +16,7 @@ students = [
 # print the list of students
 puts "The students of Villians Academy"
 puts "-----------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |student| puts student }
 
 # print total number of students
 puts  "Overall, we have #{students.count} great students"
