2732f235c35c5b1b5a7a46c961e63deada70d3b7
Alex Withington-Smith <alex@awithsmith.com>
2020-10-11 10:39:36 +0100

    each method replaced with until in #input_students

--- directory.rb
+++ directory.rb
@@ -6,7 +6,7 @@ def input_students
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
-  until name.empty? do
+  until name.empty?
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
