2f8d9ec4717904d0b269185b557f5031fad3c007
Alex Withington-Smith <alex@awithsmith.com>
2020-10-12 08:54:56 +0100

    added student list to a class

--- directory.rb
+++ directory.rb
@@ -1,39 +1,47 @@
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create empty array
-  students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  until name.empty?
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+class StudentList
+  attr_reader :students
+
+  def initialize
+    @students = []
+  end
+
+  def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create empty array
+    # get the first name
     name = gets.chomp
+    # while the name is not empty, repeat this code
+    until name.empty?
+      # add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+    end
+    # return array of students
+    students
   end
-  # return array of students
-  students
-end
 
-# print the list of students
-def print_header
-  puts "The students of Villians Academy"
-  puts "-----------------"
-end
+  # print the list of students
+  def print_header
+    puts "The students of Villians Academy"
+    puts "-----------------"
+  end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "Student #{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
+  def print_students
+    students.each_with_index do |student, index|
+      puts "Student #{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+
+  def print_footer
+    puts "Overall, we have #{students.count} great students"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = StudentList.new
+students.input_students
+students.print_header
+students.print_students
+students.print_footer
